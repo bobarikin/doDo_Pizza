b9fd71063dc931547272a23392e9536e2a8ef7b9
Artyom <42605576+bobarikin@users.noreply.github.com>
2021-12-23 16:55:13 +0300

    modal window pizza add

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,19 @@
-
 const openButton = document.querySelector(".openModal");
 const closeButton = document.querySelector(".close");
 const popUp = document.querySelector(".popup");
 
-openButton.addEventListener("click", function() {
-    popUp.style.display = "block";
-    document.body.style.overflow = "hidden";
-});
+openButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
+popUp.addEventListener("click", closeModal);
 
-closeButton.addEventListener("click", function() {
-    popUp.style.display = "none";
-    document.body.style.overflow = "";
-});
+function openModal() {
+  popUp.style.display = "block";
+  document.body.style.overflow = "hidden";
+  document.querySelector("#header_sticky").style.display = "none";
+}
+
+function closeModal() {
+  popUp.style.display = "none";
+  document.body.style.overflow = "";
+  document.querySelector("#header_sticky").style.display = "flex";
+}
